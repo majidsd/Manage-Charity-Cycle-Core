297009c70bebe93e53ee2c7f65a2ccb352d48def
Abdalmajid Abdalla <abdalmaged.abdalla@gmail.com>
2022-01-24 18:47:40 +0100

    Remove created_by from the query, it's not in the wallet table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT IGNORE INTO   user   (id,status,enabled,created_at,last_update) VALUES ('1','1',1,'2020-06-06','2020-06-06')
-INSERT IGNORE INTO wallet   (id,created_at,current_balance,last_update,created_by) VALUES (1,'2020-06-06',0.0,'2020-06-06',1)
+INSERT IGNORE INTO wallet   (id,created_at,current_balance,last_update) VALUES (1,'2020-06-06',0.0,'2020-06-06')
 UPDATE user SET wallet_id = 1 WHERE id = 1
 INSERT IGNORE INTO authority(id,authority_name,authority_description) VALUES (1,'ANONYMOUS','The anonymous user authority')
 INSERT IGNORE INTO authority(id,authority_name,authority_description) VALUES (2,'USER','The ordinary user authority')
